4f64745681837e926a646d48ceb3a42c3a356eef
fer221 <fernanda.1991.alejandra@gmail.com>
2020-09-07 15:56:13 -0300

    agragando router para paginas

--- holaMundo/src/app/app-routing.module.ts
+++ holaMundo/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+import { from } from 'rxjs';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+  { path: '', redirectTo:'/login', pathMatch: 'full'},
+  { path: 'login', component: LoginComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full'},
+  { path: 'home', component: HomeComponent}
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
